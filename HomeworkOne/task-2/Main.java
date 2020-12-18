bed0cd0d83562cbc73e7967106f357651de471f1
Siarhei Bordak <siarheibo@gmail.com>
2020-12-18 20:57:37 +0300

    completed the second task

---
+++ HomeworkOne/task-2/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+
+    public static void main(String[] args) {
+	    byte b = 127;
+	    short s = 32767;
+	    int i = 2147483647;
+	    long l = 9_223_372_036_854_775_807L;
+	    double d = 8.5;
+	    float f = 8.5F;
+	    boolean b1 = true;
+	    char c = 'A';
+	    String str = "Hello World!";
+    }
+}
