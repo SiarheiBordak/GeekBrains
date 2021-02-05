265ceb2f3ffd0dc5cf3155767f8845c136cd6fe7
Siarhei Bordak <76175381+SiarheiBordak@users.noreply.github.com>
2021-02-05 19:07:42 +0300

    Update Main.java

--- HomeworkTenth/task-1/Main.java
+++ HomeworkTenth/task-1/Main.java
@@ -37,7 +37,7 @@ public class Main {
 //            System.out.println(method(arraysTwo));
 //            System.out.println(method(arraysThree));
         } catch (MyArraySizeException | MyArrayDataException e) {
-            System.out.println(e);
+            e.printStackTrace();
         }
     }
 
